47b7fa23e1420cd537f473327ec76e0e2200854c
4965 <you@example.com>
2023-03-23 16:44:10 -0700

    -

---
+++ my_directory/Orbit_Propagator.py
@@ -0,0 +1,105 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import ode
+import planatary_data as pd
+
+class orbitpropagator:
+    def _init_(self,r0,v0,tspan,dt,cb=pd.earth):
+        self.r0=r0
+        self.v0=v0
+        self.tspan=tspan
+        self.dt=dt
+        self.cb=cb
+
+    def prograte_orbit(self):
+
+        # total number of steps
+        self.n_steps = int(np.ceil(self.tspan /self. dt))
+
+        # initialize arrays
+        self.ys = np.zeros((self.n_steps, 6))
+        self.ts = np.zeros((self.n_steps, 1))
+
+        # initial conditions
+        self.y0 = self.r0 + self. v0
+        self.ys[0] =self. np.array(y0)
+        self.step = 1
+
+        # initial solver
+        self.solver = ode(self.diffy_q)
+        self.solver.set_integrator('lsoda')
+        self.solver.set_initial_value(self.y0, 0)
+
+
+        # propagate orbit
+        while self.solver.successful() and self.step <self.n_steps:
+             self.solver.integrate(self.solver.t + self.dt)
+             self.ts[self.step] = self.solver.t
+             self.ys[self.step] = self.solver.y
+              self.step += 1
+
+        self.rs = self.ys[:, :3]
+        self.vs = self.ys[:, 3:]
+    def diffy_q(self,t, y):
+        # unpack state 
+        rx, ry, rz, vx, vy, vz = y
+        r = np.array([rx, ry, rz])
+
+        # norm of the radius vector
+        norm_r = np.linalg.norm(r)
+
+        # two body acceleration
+        ax, ay, az = -r*self.self.cb['mu'] / norm_r**3
+
+        return [vx, vy, vz, ax, ay, az]
+    def plot_3d(self,show_plot=false,save_plot=false):
+        fig=plt.figure(figsize=(16,8))
+        ax=fig.add_subplot(111,projection='3d')
+    
+    
+
+        # Extract the x and y coordinates from the rs array
+        x = rs[0, 0]
+        y = rs[0, 2]
+
+        # Plot the initial position
+        ax.plot([x], [y], 'wo', label='initialposition')
+
+
+        #plot central body
+        _u,_v=np.mgrid[0:2*np.pi:20j,0:np.pi:10j]
+        _x=earth_radius*np.cos(_u)*np.sin(_v)
+        _y=earth_radius*np.sin(_u)*np.sin(_v)
+        _z=earth_radius*np.cos(_v)*np.sin(_v) 
+        ax.plot_surface(_x,_y,_z,cmap='Blues')
+    
+        #plot the x,y,z vectors
+        1==earth_radius*2
+        x,y,z=[[0,0,0],[0,0,0],[0,0,0]]
+        u,v,w=[[1,0,0],[0,1,0],[0,0,1]]
+        ax.quiver(x,y,z,u,v,w,color='k')
+    
+        max_val=np.max(np.abs(rs))
+        ax.set_xlim([-max_val,max_val])
+        ax.set_ylim([-max_val,max_val])
+        ax.set_zlim([-max_val,max_val])
+    
+        ax.set_xlabel(['x(Km)'])
+        ax.set_ylabel(['y(Km)'])
+        ax.set_zlabel(['z(Km)'])
+    
+        #ax.set_aspect('equal')
+        ax.set_title('Example title')
+        plt.legend()
+        plt.show()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
---
+++ my_directory/planetary_data.py
@@ -0,0 +1,17 @@
+sun = {
+    'name': 'sun',
+    'mass': 1.989e30,
+    'mu': 1.32712e11,
+    'radius': 695700.0
+}
+
+earth = {
+    'name': 'earth',
+    'mass': 5.972e4,
+    'mu': 5.972e4*G,
+    'radius': 6378.0,
+}
+
+
+
+
